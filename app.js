718d8b16b630d1eab162a177d56a1511c73d8411
你见唔到我 <403482009@qq.com>
2019-08-21 18:38:01 +0800

    first Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+
+const app = express()
+app.use(express.static(__dirname + '/static'))
+
+
+app.listen(3000,function(){
+    console.log('Example app listening on port 3000!');
+})
